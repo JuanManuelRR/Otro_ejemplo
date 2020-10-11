50d18f938df40f74994fc828bb49dfd752ca14aa
JuanManuelRR <juanma.yyhyu112@gmail.com>
2020-10-11 20:35:54 -0300

    Se crea clase Persona

---
+++ src/main/java/Entidades/Persona.java
@@ -0,0 +1,14 @@
+package Entidades;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Persona {
+    private String nombre;
+}
